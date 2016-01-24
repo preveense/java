006a27670d8d8194daf972fc0dfeb9c48b20b0bb
Preveen Padmanabhan <ppadmanabhan@sunedison.com>
2016-01-24 21:58:46 +0530

    Update IMEI.java

--- IMEI.java
+++ IMEI.java
@@ -1,4 +1,14 @@
 public class {
-
+  
+  
+  int sofd(String s) {
+    int i; int sum=0;
+    for(i=0 ; i<14 ; i++) {
+     int d; 
+     d =  (i%2 != 0) ? 2 *Integer.parseInt(s.substring(i,i+1)) : Integer.parseInt(s.substring(i,i+1));
+     sum+= (d>9) ? (d/10 + d%10) : d;
+    } 
+    return sum;
+  }
 
 }
