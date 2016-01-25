8d258604cbde0c17c9442071790911c0044aa8c5
Preveen Padmanabhan <ppadmanabhan@sunedison.com>
2016-01-25 08:40:20 +0530

    Update IMEI.java

--- IMEI.java
+++ IMEI.java
@@ -5,7 +5,8 @@ public class {
     int i; int sum=0;
     for(i=0 ; i<14 ; i++) {
      int d; 
-     d =  (i%2 != 0) ? 2 *Integer.parseInt(s.substring(i,i+1)) : Integer.parseInt(s.substring(i,i+1));
+     //d =  (i%2 != 0) ? 2 *Integer.parseInt(s.substring(i,i+1)) : Integer.parseInt(s.substring(i,i+1));
+     d = Integer.parseInt( s.substring( i, i+1 )) * ( ( i % 2 != 0 ) ? 2 : 1 );
      sum+= (d>9) ? (d/10 + d%10) : d;
     } 
     return sum;
